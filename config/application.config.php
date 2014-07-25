9bb4fea7bae1d8cef47e896e5e917b3b4ee30f4a
Matthew Setter <matthew.setter@gmail.com>
2014-07-25 17:03:47 +0200

    Create the core of the VideoHoster module
    I'm starting simple by starting with the static business pages (faq etc).
    Despite the simplicity, basic tests are available to cover the
    functionality. I expect to add more in time, but for the moment, there's
    no need.

--- config/application.config.php
+++ config/application.config.php
@@ -8,6 +8,7 @@
 return array(
     'modules' => array(
         'Application',
+        'VideoHoster'
     ),
     'module_listener_options' => array(
         'module_paths' => array(
---
+++ module/VideoHoster/test/VideoHosterTest/Controller/BusinessPagesControllerTest.php
@@ -0,0 +1,144 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Provides tests for the business pages
+ *
+ * These shouldn't really be that comples as the pages are just simple text.
+ *
+ * PHP version 5.4+
+ *
+ * @category   VideoHosterTest
+ * @package    Controller
+ * @author     Matthew Setter <matthew@maltblue.com>
+ * @copyright  2014 matthew@maltblue.com
+ * @since      File available since Release/Tag: 1.0
+ */
+
+namespace VideoHosterTest\Controller;
+
+use VideoHosterTest\Bootstrap;
+use Zend\Mvc\Router\Http\TreeRouteStack as HttpRouter;
+use VideoHoster\Controller\BusinessPagesController;
+use Zend\Http\Request;
+use Zend\Http\Response;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
+use PHPUnit_Framework_TestCase;
+
+class BusinessPagesControllerTest extends \PHPUnit_Framework_TestCase
+{
+    protected $controller;
+    protected $request;
+    protected $response;
+    protected $routeMatch;
+    protected $event;
+
+    protected function setUp()
+    {
+        $serviceManager = Bootstrap::getServiceManager();
+        $this->controller = new BusinessPagesController();
+        $this->request    = new Request();
+        $this->routeMatch = new RouteMatch(array('controller' => 'index'));
+        $this->event      = new MvcEvent();
+        $config = $serviceManager->get('Config');
+        $routerConfig = isset($config['router']) ? $config['router'] : array();
+        $router = HttpRouter::factory($routerConfig);
+
+        $this->event->setRouter($router);
+        $this->event->setRouteMatch($this->routeMatch);
+        $this->controller->setEvent($this->event);
+        $this->controller->setServiceLocator($serviceManager);
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'index');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testAboutActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'about');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testFaqActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'faq');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testTestimonialsActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'testimonials');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testSupportActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'support');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testPrivacyActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'privacy');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testCopyrightActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'copyright');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testDisclaimerActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'disclaimer');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testTermsActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'terms');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+}
