e50948855082e4e532b9db96759bc972dac28c4f
Matthew Setter <matthew.setter@gmail.com>
2014-08-03 07:32:30 +0200

    Make code more descriptive with some function comments

--- module/VideoHoster/src/VideoHoster/Controller/VideosController.php
+++ module/VideoHoster/src/VideoHoster/Controller/VideosController.php
@@ -8,8 +8,21 @@ use Zend\View\Model\ViewModel;
 
 class VideosController extends AbstractActionController
 {
+    /**
+     * Provides connection to the videos table in the database
+     *
+     * @var \VideoHoster\Tables\VideoTable videoTable
+     * @access protected
+     */
     protected $videoTable;
 
+    /**
+     *  Basic class constructor
+     *
+     * @param \VideoHoster\Tables\VideoTable $videoTable
+     * @access public
+     * @return void
+     */
     public function __construct(VideoTable $videoTable)
     {
         $this->videoTable = $videoTable;
