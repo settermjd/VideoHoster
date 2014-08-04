f964fc42fb32876e57cd7b6ded0ad2745715a1b7
Matthew Setter <matthew.setter@gmail.com>
2014-08-04 16:20:36 +0200

    Replace the original videos controller test with the advanced one

--- module/VideoHoster/test/VideoHosterTest/Controller/VideosControllerTest.php
+++ module/VideoHoster/test/VideoHosterTest/Controller/VideosControllerTest.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: mattsetter
  * Date: 28/07/14
- * Time: 21:02
+ * Time: 21:09
  */
 
 namespace VideoHosterTest\Controller;
@@ -11,47 +11,128 @@ namespace VideoHosterTest\Controller;
 use VideoHosterTest\Bootstrap;
 use Zend\Mvc\Router\Http\TreeRouteStack as HttpRouter;
 use VideoHoster\Controller\VideosController;
+use VideoHoster\Tables\VideoTable;
+use Zend\Server\Method\Parameter;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 use Zend\Http\Request;
 use Zend\Http\Response;
 use Zend\Mvc\MvcEvent;
 use Zend\Mvc\Router\RouteMatch;
-use PHPUnit_Framework_TestCase;
+use Faker;
 
-class VideosControllerTest extends \PHPUnit_Framework_TestCase
+class VideosControllerTest extends AbstractHttpControllerTestCase
 {
-    protected $controller;
-    protected $request;
-    protected $response;
-    protected $routeMatch;
-    protected $event;
-
-    protected function setUp()
+    public function setUp()
     {
-        $serviceManager = Bootstrap::getServiceManager();
-        $this->controller = new VideosController($serviceManager->get(
-            'VideoHoster\Tables\VideoTable'
-        ));
-        $this->request    = new Request();
-        $this->routeMatch = new RouteMatch(array('controller' => 'index'));
-        $this->event      = new MvcEvent();
-        $config = $serviceManager->get('Config');
-        $routerConfig = isset($config['router']) ? $config['router'] : array();
-        $router = HttpRouter::factory($routerConfig);
+        $this->setApplicationConfig(
+            include __DIR__ . '/../../../../../config/application.config.php'
+        );
+        parent::setUp();
 
-        $this->event->setRouter($router);
-        $this->event->setRouteMatch($this->routeMatch);
-        $this->controller->setEvent($this->event);
-        $this->controller->setServiceLocator($serviceManager);
+        $mockTable = \Mockery::mock('VideoHoster\Tables\VideoTable');
+
+        $serviceManager = $this->getApplicationServiceLocator();
+        $serviceManager->setAllowOverride(true);
+        $serviceManager->setService(
+            'VideoHoster\Tables\VideoTable', $mockTable
+        );
+    }
+        );
     }
 
-    public function testIndexActionCanBeAccessed()
+    /**
+     * @dataProvider validCategoryProvider
+     */
+    public function testCanDispatchToValidCategoryPages($validCategory)
     {
-        $this->routeMatch->setParam('action', 'index');
-
-        $result   = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->dispatch('/tutorials/category/' . $validCategory);
+        $this->assertResponseStatusCode(200);
     }
 
+    public function validCategoryProvider()
+    {
+        $data = array();
+        $faker = Faker\Factory::create();
+        foreach(range(1, 100) as $count) {
+            $data[] = array($faker->slug());
+        }
+
+        return $data;
+    }
+
+    /**
+     * @dataProvider invalidCategoryProvider
+     */
+    public function testCannotDispatchToInvalidCategoryPages($validCategory)
+    {
+        $this->dispatch('/tutorials/category/' . $validCategory);
+        $this->assertResponseStatusCode(404);
+    }
+
+    public function invalidCategoryProvider()
+    {
+        return array(
+            array('security01'),
+            array('service+manager'),
+            array('21forms'),
+            array('module-manager£@!'),
+            array('input_filter.,/?><')
+        );
+    }
+
+    /**
+     * @dataProvider validSkillLevelProvider
+     */
+    public function testCanDispatchToValidSkillLevelPages($validSkill)
+    {
+        $this->dispatch('/tutorials/skill/' . $validSkill);
+        $this->assertResponseStatusCode(200);
+    }
+
+    public function validSkillLevelProvider()
+    {
+        return array(
+            array('beginner'),
+            array('intermediate'),
+            array('advanced'),
+        );
+    }
+
+    /**
+     * @dataProvider invalidSkillLevelProvider
+     */
+    public function testCannotDispatchToInvalidSkillLevelPages($validSkill)
+    {
+        $this->dispatch('/tutorials/skill/' . $validSkill);
+        $this->assertResponseStatusCode(404);
+    }
+
+    public function invalidSkillLevelProvider()
+    {
+        return array(
+            array('starting'),
+            array('legend'),
+            array('l33th@x0r'),
+        );
+    }
+
+    /**
+     * @dataProvider validVideoPagesProvider
+     */
+    public function testCanDispatchToValidVideoPages($validPages)
+    {
+        $this->dispatch('/' . $validPages);
+        $this->assertResponseStatusCode(404);
+    }
+
+    public function validVideoPagesProvider()
+    {
+        $data = array();
+        $faker = Faker\Factory::create();
+        foreach(range(1, 100) as $count) {
+            $data[] = array($faker->slug());
+        }
+
+        return $data;
+    }
 } 
