58464aa4053a8cacf98852167e0cecfa90532fe1
Matthew Setter <matthew.setter@gmail.com>
2014-11-12 08:24:50 +0100

    Add login link to layout
    The application layout file was copied to the videohoster module and
    a link to the login route was added in the navigation menu. The
    application layout was overridden as I don't want to pollute the
    module. I want to keep as much of the work in the videohoster module
    as possible.

--- module/VideoHoster/config/module.config.php
+++ module/VideoHoster/config/module.config.php
@@ -84,6 +84,7 @@ return array(
         ),
         'template_map' => array(
             'video-results' => __DIR__ . '/../view/video-hoster/videos/partials/video-results.phtml',
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
         )
     ),
 );
