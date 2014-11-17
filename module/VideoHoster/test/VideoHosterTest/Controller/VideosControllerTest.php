19e02fcb7f5c1dda62efdbaecb5d972b61829909
Matthew Setter <matthew.setter@gmail.com>
2014-11-17 08:36:47 +0100

    Implement basic view video functionality
    The tests and code have been added to implement a basic view video
    page. There's not much error handling, but it works, for now.

--- module/VideoHoster/test/VideoHosterTest/Controller/VideosControllerTest.php
+++ module/VideoHoster/test/VideoHosterTest/Controller/VideosControllerTest.php
@@ -128,4 +128,97 @@ class VideosControllerTest extends AbstractHttpControllerTestCase
 
         return $data;
     }
+
+    public function testCanDispatchToViewVideoPages()
+    {
+        $video = new VideoModel();
+        $slug = 'freddie-mercury-live';
+        $video->exchangeArray(array(
+            'videoId' => 12,
+            'name' => "Freddie Mercury Live",
+            'slug' => "freddie-mercury-live",
+            'description' => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+            'authorId' => 1,
+            'statusId' => 1,
+            'paymentRequirementId' => 1,
+            'extract' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+            'duration' => 115,
+            'publishDate' => '2008-08-01',
+            'publishTime' => '11:15',
+            'levelId' => 1
+        ));
+
+        $mockTable = \Mockery::mock('VideoHoster\Tables\VideoTable');
+        $mockTable->shouldReceive('fetchBySlug')
+            ->once()
+            ->andReturn($video);
+
+        $serviceManager = $this->getApplicationServiceLocator();
+        $serviceManager->setAllowOverride(true);
+        $serviceManager->setService(
+            'VideoHoster\Tables\VideoTable', $mockTable
+        );
+
+        $this->dispatch('/videos/' . $slug);
+        $this->assertResponseStatusCode(200);
+    }
+
+    public function testRedirectToIndexPageIfVideoNotAvailableOnViewPage()
+    {
+        $video = new VideoModel();
+        $slug = 'freddie-mercury-live';
+
+        $mockTable = \Mockery::mock('VideoHoster\Tables\VideoTable');
+        $mockTable->shouldReceive('fetchBySlug')
+            ->once()
+            ->andReturn(null);
+
+        $serviceManager = $this->getApplicationServiceLocator();
+        $serviceManager->setAllowOverride(true);
+        $serviceManager->setService(
+            'VideoHoster\Tables\VideoTable', $mockTable
+        );
+
+        $this->dispatch('/videos/' . $slug);
+        $this->assertRedirect();
+        $this->assertActionName('ViewVideo');
+        $this->assertControllerClass('VideosController');
+        $this->assertMatchedRouteName('videos/view-video');
+    }
+
+    public function testCanViewVideoIfAvailable()
+    {
+        $video = new VideoModel();
+        $video->exchangeArray(array(
+            'videoId' => 12,
+            'name' => "Freddie Mercury Live",
+            'slug' => "freddie-mercury-live",
+            'description' => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+            'authorId' => 1,
+            'statusId' => 1,
+            'paymentRequirementId' => 1,
+            'extract' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+            'duration' => 115,
+            'publishDate' => '2008-08-01',
+            'publishTime' => '11:15',
+            'levelId' => 1
+        ));
+        $slug = 'freddie-mercury-live';
+
+        $mockTable = \Mockery::mock('VideoHoster\Tables\VideoTable');
+        $mockTable->shouldReceive('fetchBySlug')
+            ->once()
+            ->andReturn($video);
+
+        $serviceManager = $this->getApplicationServiceLocator();
+        $serviceManager->setAllowOverride(true);
+        $serviceManager->setService(
+            'VideoHoster\Tables\VideoTable', $mockTable
+        );
+
+        $this->dispatch('/videos/' . $slug);
+        $this->assertActionName('ViewVideo');
+        $this->assertControllerClass('VideosController');
+        $this->assertMatchedRouteName('videos/view-video');
+    }
 }
